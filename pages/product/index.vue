79426ccbfc36b3c3d87646cfc0dfac3107623b1a
Varez Luniansyah <varezluniansyah08@gmail.com>
2023-11-24 13:20:38 +0700

    pesan

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 import { useProductsStore } from "~/stores/products";
 
+definePageMeta({
+    middleware: ["user-access"]
+});
+
 const productStore = useProductsStore();
 const allProducts = ref([]);
 
@@ -17,10 +21,10 @@ const selectedCategory = ref("");
             <div class="py-10">
                 <div class="mb-6 flex justify-end gap-6">
                     <NuxtLink to="/category/create" class="bg-orange-500 text-white flex justify-
-center items-center px-3 rounded-lg">Create Category
+                    center items-center px-3 rounded-lg">Create Category
                     </NuxtLink>
                     <NuxtLink to="/product/create" class="bg-green-500 text-white flex justify-center
-items-center px-3 rounded-lg">Create Products
+                    items-center px-3 rounded-lg">Create Products
                     </NuxtLink>
                     <Dropdown @selected-category="selectedCategory = $event" />
                 </div>
